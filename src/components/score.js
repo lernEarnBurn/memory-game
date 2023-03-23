6bc734f283e308d1a781516d2f6d5bc4e2e86794
onetimecircle <lerners124@gmail.com>
2023-03-23 19:04:32 -0400

    styled scoreboard

--- src/components/score.js
+++ src/components/score.js
@@ -5,7 +5,20 @@ export function Scoreboard(){
 
     return (
         <div className="score-container">
-            
+            <div className="score current">
+                <label className="top">Current Score</label>
+                2
+                </div> 
+            <div className="score high">
+                <label className="bottom">High Score</label>
+                11
+                </div>
+            <div className="instructions">
+                <p>
+                    Pick a card without picking one you already picked.
+                    Once you go through each card once, you can pick any card.
+                </p>
+            </div>
         </div>
     )
 }
