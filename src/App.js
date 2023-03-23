a6d59807568a7c741b18b96bf1b36f7bf15df787
onetimecircle <lerners124@gmail.com>
2023-03-22 20:10:17 -0400

    ui progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './app.css'
 import { Board } from "./components/board";
 import { Scoreboard } from "./components/score";
 
+//since this is global may make it unable to alter this
+const cardPaths = [
+                  "../images/brian.png",
+                  "../images/chris.jpeg",
+                  "../images/joe.jpeg",
+                  "../images/louis.jpeg",
+                  "../images/moe.jpeg",
+                  "../images/mort.jpeg",
+                  "../images/peter.jpeg",
+                  "../images/stewie.png",
+                  "../images/quagmire.jpeg",
+                  "../images/meg.png"
+                ]
+
 function App() {
+
+  const [cards, cardAmount] = useState(cardPaths)
+
   return (
     <React.Fragment>
       <div className="top-bar">
@@ -11,7 +28,7 @@ function App() {
       </div>
       <div className="content-container">
         <Scoreboard/>
-        <Board/>
+        <Board cardPaths={cards}/>
       </div>
     </React.Fragment>
   );
--- src/components/board.js
+++ src/components/board.js
@@ -1,9 +1,19 @@
 import React from "react";
 import '../css/board.css'
+import uniqid from 'uniqid'
 
-export function Board(){
+export function Board(props){
 
     return (
-        <div className="board"></div>
+        <div className="board">
+
+            <ul className="card-container">
+                {props.cardPaths.map((path) => {
+                    return  <li>
+                                <div key={uniqid()} className="card" style={{ backgroundImage: `url(${path})` }}></div>
+                            </li>
+                })}
+            </ul>
+        </div>
     )
 }
