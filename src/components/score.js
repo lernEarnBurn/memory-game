7048871732ba29f757e112d6324351fdb6d74060
onetimecircle <lerners124@gmail.com>
2023-03-23 21:51:10 -0400

    ready to implement the functionality

--- src/components/score.js
+++ src/components/score.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import '../css/score.css'
 
+
+
 export function Scoreboard(){
+    const [currentScore, scoreChanger] = useState(0)
+    //possibly use a json file so high score doesn't reset
+    const [highScore, scoreChanger2] = useState(0)
+
+    
 
     return (
         <div className="score-container">
-            <div className="score current">
+            <div className="score current" >
                 <label className="top">Current Score</label>
-                2
+                {currentScore}
                 </div> 
             <div className="score high">
                 <label className="bottom">High Score</label>
-                11
+                {highScore}
                 </div>
             <div className="instructions">
                 <p>
